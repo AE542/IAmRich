0f52003b7bb816260526d7c3b2d6c4c891fc509b
AE542 <65149031+AE542@users.noreply.github.com>
2021-02-04 23:55:38 +0900

    Add files via upload
    Basic App showing a screen also added custom app icons made on canva.

---
+++ I am Rich App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am Rich App
+//
+//  Created by Mohammed Qureshi on 2020/07/06.
+//  Copyright © 2020 Experiment1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
